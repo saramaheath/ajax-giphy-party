b0f0a1194b1b0699e6550fe4d8103d63ed37bbc9
Sara Heath <saramaheath@gmail.com>
2022-05-31 18:57:35 -0500

    add delete button to html and js docs

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ console.log("Let's get this party started!");
 const $container = $(".gif-storage");
 const $button = $("#goButton");
 
-let $keyword = $("search-term");
-let keyword = $keyword.eq(0).val();
+let $keyword = $(".search-term");
+let $delete = $("#deleteAll");
 
 /** Gets a gif from giphy */
-async function getGif(){
-    let gif = await axios.get(`http://api.giphy.com/v1/gifs/search?`, {params: {q: `${keyword}`, api_key: "Nlgp4981igjH8Q1KLPDfqo0EyHEurTaI"}});
-    let rawGif = gif.data.data[0].url;
-    console.log("getGif=", gif.data.data[0].url);
-    addGif(rawGif);
+async function getGif(keyword) {
+  let gif = await axios.get(`http://api.giphy.com/v1/gifs/search?`, {
+    params: { q: `${keyword}`, api_key: "Nlgp4981igjH8Q1KLPDfqo0EyHEurTaI" },
+  });
+  let rawGif = gif.data.data[0].embed_url;
+  console.log("getGif=", gif.data.data[0].embed_url);
 
+  addGif(rawGif);
 }
 /** adds gif to our gif storage in html */
-function addGif(gif){
-    $container.append(`<img src = "${gif}">`)
+function addGif(gif) {
+  $container.append(`<iframe src = "${gif}">`);
 }
 /** On button click, we get a gif from giphy */
-$button.on('click', function(e){
+$button.on("click", function (e) {
     e.preventDefault();
-    getGif();
+    let keyword = $keyword.eq(0).val();
+    console.log(keyword, 'keyword');
+  getGif(keyword);
+});
+
+$delete.on("click", function (e) {
+    $container.empty();
 })
+
