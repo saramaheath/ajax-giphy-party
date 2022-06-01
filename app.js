189ad2bc92dd2e58227b166e702cfe3542679d73
Sara Heath <saramaheath@gmail.com>
2022-05-31 19:00:57 -0500

    docstring edits

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+
 console.log("Let's get this party started!");
 const $container = $(".gif-storage");
 const $button = $("#goButton");
@@ -6,7 +7,7 @@ const $button = $("#goButton");
 let $keyword = $(".search-term");
 let $delete = $("#deleteAll");
 
-/** Gets a gif from giphy */
+/** Gets a gif from giphy using key word from form, calls addGif func  */
 async function getGif(keyword) {
   let gif = await axios.get(`http://api.giphy.com/v1/gifs/search?`, {
     params: { q: `${keyword}`, api_key: "Nlgp4981igjH8Q1KLPDfqo0EyHEurTaI" },
@@ -20,14 +21,14 @@ async function getGif(keyword) {
 function addGif(gif) {
   $container.append(`<iframe src = "${gif}">`);
 }
-/** On button click, we get a gif from giphy */
+/** On button click, we get a gif from giphy, calls getGif func */
 $button.on("click", function (e) {
     e.preventDefault();
     let keyword = $keyword.eq(0).val();
     console.log(keyword, 'keyword');
   getGif(keyword);
 });
-
+/** On button click, remove all current gifs from container */
 $delete.on("click", function (e) {
     $container.empty();
 })
