16ae16b67533894744ea0d7957fc6ccbfa80e1e2
Sara Heath <saramaheath@gmail.com>
2022-05-31 19:23:26 -0500

    changed iframe tag to image tag, updated rawGif value to access original gif url

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ async function getGif(keyword) {
   let gif = await axios.get(`http://api.giphy.com/v1/gifs/search?`, {
     params: { q: `${keyword}`, api_key: "Nlgp4981igjH8Q1KLPDfqo0EyHEurTaI" },
   });
-  let rawGif = gif.data.data[0].embed_url;
+  console.log(gif.data.data);
+  let rawGif = gif.data.data[0].images.original.url;
   console.log("getGif=", gif.data.data[0].embed_url);
 
   addGif(rawGif);
 }
 /** adds gif to our gif storage in html */
 function addGif(gif) {
-  $container.append(`<iframe src = "${gif}">`);
+  $container.append(`<image src = "${gif}">`);
 }
 /** On button click, we get a gif from giphy, calls getGif func */
 $button.on("click", function (e) {
